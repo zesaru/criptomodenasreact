574c426266b2ad0bd2ba4f29e86c60da25436736
Cesar Murillo <namiayumis@gmail.com>
2020-02-14 13:02:04 +0900

    agregando formulario

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "@emotion/styled";
 import imagen from "./cryptomonedas.png";
+import Formulario from "./components/Formulario";
 
 const Contenedor = styled.div`
   max-width: 900px;
@@ -40,6 +41,7 @@ function App() {
       </div>
       <div>
         <Heading>Cotiza Criptomonedas al Instante</Heading>
+        <Formulario />
       </div>
     </Contenedor>
   );
