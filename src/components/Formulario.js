e64491e9481dbc31cacbf416271cdf220dac4d4d
Cesar Murillo <namiayumis@gmail.com>
2020-02-17 11:00:44 +0900

    mostrando el mensaje de error

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -3,6 +3,7 @@ import styled from "@emotion/styled";
 import useMoneda from "../hooks/useMoneda";
 import useCriptomoneda from "../hooks/useCriptomoneda";
 import axios from "axios";
+import Error from "./Error";
 
 const Boton = styled.input`
   margin-top: 20px;
@@ -67,7 +68,7 @@ const Formulario = () => {
 
   return (
     <form onSubmit={cotizarMoneda}>
-      {error ? "Hay un error" : null}
+      {error ? <Error mensaje="Todos los campos son obligatorios" /> : null}
       <SelectMonedas />
       <SelectCripto />
       <Boton type="submit" value="Calcular" />
