a7cd0315c930a20e8db158438f9f9f7fa649121c
Cesar Murillo <namiayumis@gmail.com>
2020-02-17 21:36:29 +0900

    styled components al resultado

--- src/components/Cotizacion.js
+++ src/components/Cotizacion.js
@@ -1,28 +1,46 @@
 import React from "react";
+import styled from "@emotion/styled";
 
+const ResultadoDiv = styled.div`
+  color: #fff;
+  font-family: Arial, Helvetica, sans-serif;
+`;
+
+const Info = styled.p`
+  font-size: 18px;
+  span {
+    font-weight: bold;
+  }
+`;
+const Precio = styled.p`
+  font-size: 30px;
+  span {
+    font-weight: bold;
+  }
+`;
 const Cotizacion = ({ resultado }) => {
   if (Object.keys(resultado).length === 0) return null;
 
   console.log(resultado);
 
   return (
-    <div>
-      <p>
+    <ResultadoDiv>
+      <Precio>
         El precio es: <span>{resultado.PRICE}</span>{" "}
-      </p>
-      <p>
+      </Precio>
+      <Info>
         Precio más alto del día: <span>{resultado.HIGHDAY}</span>{" "}
-      </p>
-      <p>
+      </Info>
+      <Info>
         Precio más bajo del día: <span>{resultado.LOWDAY}</span>{" "}
-      </p>
-      <p>
+      </Info>
+      <Info>
         Variación últimas 24 horas: <span>{resultado.CHANGEPCT24HOUR}</span>{" "}
-      </p>
-      <p>
+      </Info>
+      <Info>
         Última Actualización: <span>{resultado.LASTUPDATE}</span>{" "}
-      </p>
-    </div>
+      </Info>
+    </ResultadoDiv>
   );
 };
 
